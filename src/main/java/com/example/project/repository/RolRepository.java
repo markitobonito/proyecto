4e5fa3bf5d1e6db78c4e584b29848d2bc689afbf
Hugo <a20193265@pucp.edu.pe>
2025-04-24 17:04:02 -0500

    sql

--- src/main/java/com/example/project/repository/RolRepository.java
+++ src/main/java/com/example/project/repository/RolRepository.java
@@ -2,7 +2,9 @@ package com.example.project.repository;
 
 import com.example.project.entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RolRepository extends JpaRepository<Rol,Integer> {
     Rol findByRol(String rol);
 }
