8a641f5f5321d41e372abbb9340e087472b6707b
PJ-JP <a20213805@pucp.edu.pe>
2025-05-08 17:04:50 -0500

    feat: vista superadmin

--- src/main/java/com/example/project/controller/SuperAdminController.java
+++ src/main/java/com/example/project/controller/SuperAdminController.java
@@ -1,4 +1,36 @@
 package com.example.project.controller;
 
+import com.example.project.repository.ReservasRepository;
+import com.example.project.repository.superadmin.SuperAdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/superadmin")
 public class SuperAdminController {
+
+    @Autowired
+    SuperAdminRepository superAdminRepository;
+
+    @Autowired
+    ReservasRepository reservasRepository;
+
+    @GetMapping(value = {"", "/home"})
+    public String home(Model model) {
+        model.addAttribute("cantidadActivos", superAdminRepository.cantidadDeEspaciosPorIdEstado(1));
+        model.addAttribute("cantidadEnMantenimiento", superAdminRepository.cantidadDeEspaciosPorIdEstado(2));
+        model.addAttribute("cantidadPrestados", superAdminRepository.cantidadDeEspaciosPorIdEstado(3));
+        model.addAttribute("cantidadCerrados", superAdminRepository.cantidadDeEspaciosPorIdEstado(4));
+        model.addAttribute("cantidadTotal", superAdminRepository.count());
+        model.addAttribute("listaUsuarios", superAdminRepository.findAll());
+        model.addAttribute("listaReservas", reservasRepository.findAll());
+        model.addAttribute("totalReservas", reservasRepository.count());
+        model.addAttribute("ingresoTotal", reservasRepository.ingresoTotal());
+        model.addAttribute("ingresoMes", reservasRepository.ingresoMensual());
+        return "/superadmin/casa";
+    }
+
 }
